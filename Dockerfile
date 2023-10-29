ea1711fe1f95b3ca17ce867bf0ee878f49222077
Rostyslav Nakarlovich <zvesdochyot@gmail.com>
2023-10-29 17:23:01 +0000

    Added configs to dockerize Fabric Minecraft server with basic customization

--- Dockerfile
+++ Dockerfile
@@ -1,25 +1,24 @@
-# We're no longer using openjdk:17-slim as a base due to several unpatched vulnerabilities.
-# The results from basing off of alpine are a smaller (by 47%) and faster (by 17%) image.
-# Even with bash installed.     -Corbe
 FROM alpine:latest
 
 # Environment variables
-ENV EULA="" \
+ENV MC_VERSION="latest" \
+    PAPER_BUILD="latest" \
+    EULA="" \
     MC_RAM="" \
     JAVA_OPTS=""
 
-COPY pufferfish.sh .
+COPY fabric-server.sh .
 RUN apk update \
     && apk add openjdk17-jre-headless \
     && apk add bash \
     && apk add wget \
     && apk add jq \
-    && mkdir /pufferfish
+    && mkdir /fabric-server
 
 # Start script
-CMD ["bash", "./pufferfish.sh"]
+CMD ["bash", "./fabric-server.sh"]
 
 # Container setup
 EXPOSE 25565/tcp
 EXPOSE 25565/udp
-VOLUME /pufferfish
+VOLUME /fabric-server
---
+++ fabric-server.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Enter server directory
+cd fabric-server
+
+# Set nullstrings back to 'latest'
+: ${MC_VERSION:='latest'}
+: ${LOADER_VERSION:='latest'}
+
+# Get the latest Fabric installer version
+INSTALLER_VERSION=$(wget -qO - "https://meta.fabricmc.net/v2/versions/installer" | jq -r '.[0].version')
+
+# Get the latest Minecraft version
+if [[ $MC_VERSION == latest ]]
+then
+  MC_VERSION=$(wget -qO - "https://meta.fabricmc.net/v2/versions/game" | jq -r 'map(select(.stable == true)) | .[0]')
+fi
+
+# Get the latest Fabric loader version
+if [[ $LOADER_VERSION == latest ]]
+then
+  LOADER_VERSION=$(wget -qO - https://meta.fabricmc.net/v2/versions/loader/${MC_VERSION} | jq -r '.[0].loader.version')
+fi
+
+JAR_URL="https://meta.fabricmc.net/v2/versions/loader/${MC_VERSION}/${LOADER_VERSION}/${INSTALLER_VERSION}/server/jar"
+JAR_NAME="fabric-server-${MC_VERSION}-${LOADER_VERSION}-${INSTALLER_VERSION}.jar"
+
+# Update server jar if necessary
+if [[ ! -e $JAR_NAME ]]
+then
+  rm -rf *.jar
+  wget "$URL" -qO "$JAR_NAME"
+fi
+
+# Update eula.txt with current setting
+echo "eula=${EULA:-false}" > eula.txt
+
+# Add RAM allocation options to Java options if necessary
+if [[ -n $MC_RAM ]]
+then
+  JAVA_OPTS="-Xms${MC_RAM} -Xmx${MC_RAM} $JAVA_OPTS"
+fi
+
+# Start Fabric server
+exec java -server $JAVA_OPTS -jar "$JAR_NAME" nogui
